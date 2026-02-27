caba1325e8acc5c4b87228b31db5cc853c6ee88c
Abhishek Kumar <abhi2000.mishra10@gmail.com>
2026-02-27 16:13:20 +0530

    ec2

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = 5000;
 app.get("/", (req, res) => {
   res.json({
     message: "Dockerized Node App 🚀",
-    author: "Abhishek"
+    author: "Abhishek",
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on portd${PORT}`);
 });
